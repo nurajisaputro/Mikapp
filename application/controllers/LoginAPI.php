a93cd06d0b712534e27310f4bc97a6c045dc855e
nurajisaputro <nuraji486@gmail.com>
2024-02-11 18:53:50 +0700

    change

--- application/controllers/LoginAPI.php
+++ application/controllers/LoginAPI.php
@@ -2,13 +2,13 @@
     // USERNAME PASSWORD API PORT 8728 1036 8g 2s+
     $ip = "192.168.3.1";
     $username = "nurajisaputro";
-    $password = "";
+    $password = "nuraji12";
 
 
     // USERNAME PASSWORD API PORT 8728 1009
     $ip1009 = "10.10.101.1";
     $username1009 = "nuraji";
-    $password1009 = "";
+    $password1009 = "nurajisaputro123";
 
 
     // USERNAME TEST
